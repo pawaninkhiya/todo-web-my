6ea39b561c8879a105da5a5580c2206aa36228cd
pawaninkhiya <pawaninkhiya760@gmail.com>
2025-07-11 20:59:32 +0530

    console remove

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,16 @@ import { Toaster } from 'react-hot-toast'
 import AuthProvider from '@contexts/AuthProvider.tsx'
 import QueryProvider from '@contexts/QueryProvider.tsx'
 // Register service worker
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("/firebase-messaging-sw.js")
-        .then((registration) => {
-            console.log("Service Worker registered:", registration.scope);
-        })
-        .catch((err) => {
-            console.error("Service Worker registration failed:", err);
-        });
-}
+// if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker
+//         .register("/firebase-messaging-sw.js")
+//         .then((registration) => {
+//             console.log("Service Worker registered:", registration.scope);
+//         })
+//         .catch((err) => {
+//             console.error("Service Worker registration failed:", err);
+//         });
+// }
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter basename='/todo'>
